e8da934cf232f0ba5d5d97c02bad4e9672b20874
milda norkute <menulio.kiskis@gmail.com>
2017-11-25 00:20:45 +0200

    more login

--- frontend/src/components/Login/LoginComponent.js
+++ frontend/src/components/Login/LoginComponent.js
@@ -5,15 +5,23 @@ export default class Login extends Component {
     render( ){
         return(
            <div>
-           <h1>Explore your inner adverturer with our personalised reccommendations</h1>
-           <h3>Login with email</h3>
-           <div className="form-group">
-            <input className="form-control" placeholder="Email"/>
-          </div>
-          <div className="form-group">
-            <input type="password" className="form-control" placeholder="Password" />
-          </div>
-           <h3>Login with Facebook</h3>
+               <h1>
+                 Explore your inner adverturer with our personalised reccommendations
+               </h1>
+                <h3>
+                 Login with email
+                </h3>
+                <form>
+                    <div className="form-group">
+                        <input className="form-control" placeholder="Email"/>
+                    </div>
+                     <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Password" />
+                     </div>
+                     <button id="buttonstyle" type="submit" className="btn btn-success">Login</button>
+                </form>
+                 <h3>Login with Facebook</h3>
+                 <button id="" type="submit" className="Facebook">Continue with Facebook</button>
            </div>
         )
     }
