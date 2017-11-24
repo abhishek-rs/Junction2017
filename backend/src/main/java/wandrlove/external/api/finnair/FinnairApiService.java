37a6f5a56d4ca8f73c4fa06d07adeaec9c469ee7
Aleksandar Labroski <aleksandar.labroski@inoviagroup.se>
2017-11-25 01:24:44 +0200

    Service setup and models creation.

--- backend/src/main/java/wandrlove/external/api/finnair/FinnairApiService.java
+++ backend/src/main/java/wandrlove/external/api/finnair/FinnairApiService.java
@@ -4,19 +4,24 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.springframework.stereotype.Service;
 import wandrlove.external.api.finnair.InstantOffers.InstantOffers;
 import wandrlove.external.api.finnair.Offers.Offers;
 
+import javax.annotation.PostConstruct;
+import javax.ws.rs.POST;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class FinnairApiService {
 
     private static FinnairApiService instance = null;
     private InstantOffers instantOffers = null;
     private Offers offers = null;
 
-    protected FinnairApiService() {
+    @PostConstruct
+    private void initialize() {
         JacksonJsonProvider provider = new JacksonJsonProvider();
         List providers = new ArrayList();
         providers.add(provider);
@@ -28,12 +33,5 @@ public class FinnairApiService {
         org.apache.cxf.jaxrs.client.Client offersClient = WebClient.client(instantOffers);
         ClientConfiguration offersConfig = WebClient.getConfig(offersClient);
 
-    }
-
-    public static FinnairApiService getInstance() {
-        if (instance == null) {
-            instance = new FinnairApiService();
-        }
-        return instance;
     }
 }
