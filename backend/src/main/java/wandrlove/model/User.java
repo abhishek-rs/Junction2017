d90221865a9b0d4db3f0d6cfc2dda8301dd915c9
Aleksandar Labroski <aleksandar.labroski@inoviagroup.se>
2017-11-25 16:43:08 +0200

    Last changes from the backend in java.

--- backend/src/main/java/wandrlove/model/User.java
+++ backend/src/main/java/wandrlove/model/User.java
@@ -22,6 +22,10 @@ public class User {
         this.userId = userId;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
     public String getHomePlace() {
         return homePlace;
     }
