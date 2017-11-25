477d5e8a04bf0ab37d28ef70906ee9903d489865
Nevalainen Susanna <nevalas5@niksula.hut.fi>
2017-11-25 23:08:39 +0100

    style updates

--- frontend/src/components/Home/HomeComponent.jsx
+++ frontend/src/components/Home/HomeComponent.jsx
@@ -1,13 +1,17 @@
 
-import React, { Component } from 'react';    
+import React, { Component } from 'react';
 import MapComponent from '../Map/MapComponent';
 import './HomeComponent.css';
+import DatePick from '../DatePicker/DatePicker';
+import moment from 'moment';
+import Navbar from '../Navbar/Navbar';
 
 export default class HomeComponent extends Component {
     render() {
       return (
-        <div className="home"> 
+        <div className="home">
            <MapComponent />
+           <DatePick />
         </div>
       );
     }
