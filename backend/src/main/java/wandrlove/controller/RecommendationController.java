27b5fcf746aedcbfa8cdd8d2efaefa0f98bf978b
Aleksandar Labroski <aleksandar.labroski@inoviagroup.se>
2017-11-25 13:18:12 +0200

    Added get mapping in recommendations controller. Handling of data.json file.

--- backend/src/main/java/wandrlove/controller/RecommendationController.java
+++ backend/src/main/java/wandrlove/controller/RecommendationController.java
@@ -1,17 +1,23 @@
 package wandrlove.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wandrlove.model.Recommendation;
 
+import javax.ws.rs.GET;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/recommendations")
 public class RecommendationController {
 
-    public Recommendation[] getRecommendations(
-            @RequestParam(value = "userId") String userId) {
+    @GetMapping
+    public List<Recommendation> getRecommendations(
+            @RequestParam(value = "userId", defaultValue = "facebookUser") String userId) {
 
-        return null;
+        return new ArrayList<>();
     }
 }
